d53fbb8c8b510f250d00807303c302beb9d69080
Mansur Besleney <besleneymansur@gmail.com>
2023-04-05 22:04:30 +0300

    bugfix

--- Assets/Scripts/WeaponSway.cs
+++ Assets/Scripts/WeaponSway.cs
@@ -7,12 +7,12 @@ public class WeaponSway : MonoBehaviour
 
     [SerializeField] private Transform Tower;
     [SerializeField] private Transform Gun;
-    [SerializeField] private float towerSpeed;
-    [SerializeField] private float gunSpeed;
+    /* [SerializeField] private float towerSpeed;
+    [SerializeField] private float gunSpeed; */
     [SerializeField] private Camera cam;
 
-    private float towerAngle;
-    private float gunAngle;
+    /* private float towerAngle;
+    private float gunAngle; */
 
     
     void FixedUpdate()
@@ -24,16 +24,16 @@ public class WeaponSway : MonoBehaviour
     void RotateTower()
     {
         Quaternion cameraRotation = cam.transform.rotation;
-        towerAngle = cameraRotation.eulerAngles.y;
-        towerAngle = Mathf.Clamp(towerAngle, -90, 90);
+        /* towerAngle = cameraRotation.eulerAngles.y;
+        towerAngle = Mathf.Clamp(towerAngle, -90, 90); */
         Tower.localRotation = Quaternion.Euler(0, cameraRotation.eulerAngles.y, 0);
     }
 
     void RotateGun()
     {
         Quaternion cameraRotation = cam.transform.rotation;
-        gunAngle = cameraRotation.eulerAngles.x;
-        gunAngle = Mathf.Clamp(gunAngle, -45, 45);
+        /* gunAngle = cameraRotation.eulerAngles.x;
+        gunAngle = Mathf.Clamp(gunAngle, -45, 45); */
         Gun.localRotation = Quaternion.Euler(cameraRotation.eulerAngles.x, 0,0); 
     }
 }
